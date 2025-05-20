75a15e46ac947a36b5a6deab819081cd8e607866
Otavio Chaves <otaviocs2005@gmail.com>
2025-05-20 13:18:03 -0300

    first commit

---
+++ src/main/java/com/devocs/ocscommerce/OcsCommerceApplication.java
@@ -0,0 +1,13 @@
+package com.devocs.ocscommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OcsCommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OcsCommerceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devocs/ocscommerce/OcscommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devocs.ocscommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OcscommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

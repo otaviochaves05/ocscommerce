159b467440249df93adef62c316288c8730bed07
Otavio Chaves <otaviocs2005@gmail.com>
2025-06-13 16:33:41 -0300

    bean validation

--- src/main/java/com/devocs/ocscommerce/controllers/ProductController.java
+++ src/main/java/com/devocs/ocscommerce/controllers/ProductController.java
@@ -2,6 +2,7 @@ package com.devocs.ocscommerce.controllers;
 
 import com.devocs.ocscommerce.dto.ProductDTO;
 import com.devocs.ocscommerce.services.ProductService;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -32,7 +33,7 @@ public class ProductController {
     }
 
     @PostMapping
-    public ResponseEntity<ProductDTO> insert(@RequestBody ProductDTO dto){
+    public ResponseEntity<ProductDTO> insert(@Valid @RequestBody ProductDTO dto){
         dto = service.insert(dto);
         URI uri = ServletUriComponentsBuilder
                 .fromCurrentRequest()
@@ -43,7 +44,7 @@ public class ProductController {
     }
 
     @PutMapping(value = "{id}")
-    public ResponseEntity<ProductDTO> update(@PathVariable Long id, @RequestBody ProductDTO dto){
+    public ResponseEntity<ProductDTO> update(@PathVariable Long id, @Valid @RequestBody ProductDTO dto){
         dto = service.update(id, dto);
         return ResponseEntity.ok(dto);
     }
---
+++ src/main/java/com/devocs/ocscommerce/dto/FieldMessage.java
@@ -0,0 +1,21 @@
+package com.devocs.ocscommerce.dto;
+
+public class FieldMessage {
+
+    private String fieldName;
+    private String message;
+
+    public String getFieldName() {
+        return fieldName;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public FieldMessage(String fieldName, String message) {
+        this.fieldName = fieldName;
+        this.message = message;
+    }
+}
+
